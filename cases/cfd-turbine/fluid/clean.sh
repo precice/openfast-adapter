e6067024bd9de3492e35216c1318b85daef94df1
Gerasimos Chourdakis <chourdak@in.tum.de>
2024-05-16 10:33:51 +0300

    Improvements in the case cleaning scripts (#6)
    * Make clean scripts executable and add force flag
    * Add build directory to gitignore

--- cases/cfd-turbine/fluid/clean.sh
+++ cases/cfd-turbine/fluid/clean.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e -u
 
 # Source tutorial clean functions
 . $WM_PROJECT_DIR/bin/tools/CleanFunctions
@@ -6,6 +7,6 @@
 rm -rf 0 > /dev/null 2>&1
 
 cleanCase
-rm -r ./precice-profiling
-rm -r ../precice-run
+rm -rf ./precice-profiling
+rm -rf ../precice-run
 # ----------------------------------------------------------------- end-of-file
